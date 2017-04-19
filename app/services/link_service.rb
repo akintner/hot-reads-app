b4959cbad49597196382694331208394cf70f53b
Amz K <akintner@u.rochester.edu>
2017-04-19 17:10:38 -0600

    update for heroku on faraday call

--- app/services/link_service.rb
+++ app/services/link_service.rb
@@ -1,6 +1,6 @@
 class LinkService 
   def self.get_top_ten
-    response = Faraday.get("http://localhost:3000/api/v1/toplinks")
+    response = Faraday.get("https://ak-final-assessment.herokuapp.com/api/v1/toplinks")
     JSON.parse(response.body, symbolize_names: true)
   end
 end
